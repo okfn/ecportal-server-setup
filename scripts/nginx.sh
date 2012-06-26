d0c57a7cbf006aa221a766b4f6cf7b2a37fa8e02
John Glover <j@johnglover.net>
2012-06-26 12:23:04 +0100

    [2593] Add nginx cache config.

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -38,43 +38,102 @@ install_nginx () {
   echo '------------------------------------------'
   echo 'Installing nginx                          '
   echo '------------------------------------------'
+
   cat <<EOF > /etc/yum.repos.d/nginx.repo
-  
 [nginx]
 name=nginx repo
 baseurl=http://nginx.org/packages/centos/6/$ARCH/
 gpgcheck=0
 enabled=1
-  
 EOF
   
   yum update
   yum install -y nginx
   
   rename '.conf' '' /etc/nginx/conf.d/*.conf
+
+  cat <<EOF > /etc/nginx/nginx.conf
+user  nginx;
+worker_processes  1;
+
+error_log  /var/log/nginx/error.log warn;
+pid        /var/run/nginx.pid;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+
+    log_format  main  '$remote_addr - $remote_user [$time_local] '
+                      '$upstream_cache_status '
+                      '$upstream_http_cache_control '
+                      '"$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    proxy_cache_path /applications/ckan/nginx/cache levels=1:2 keys_zone=cache:30m max_size=200m;
+    proxy_temp_path /applications/ckan/nginx/proxy 1 2;
+
+    sendfile        on;
+    #tcp_nopush     on;
+
+    keepalive_timeout  65;
+
+    #gzip  on;
+
+    include /etc/nginx/conf.d/*.conf;
+}
+EOF
+
   cat <<EOF > /etc/nginx/conf.d/default.conf
 server {
-
   listen 80;
   server_name $CKAN_DOMAIN;
-  access_log  /var/log/nginx/$CKAN_DOMAIN.access.log;
+  access_log /var/log/nginx/$CKAN_DOMAIN.access.log main;
 
   location /open-data/elastic/ {
     internal;
-    # location of elastic search
     proxy_pass http://${CKAN_BACKEND_SERVER}:9200/;
     proxy_set_header Host \$host;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
   }
 
-   location /open-data/ {
+  location /open-data/data {
      proxy_pass http://0.0.0.0:8008;
-     proxy_set_header Host \$host:80;
-     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-     proxy_set_header X-Scheme \$scheme;
+     proxy_set_header Host $host:80;
+     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+     proxy_set_header X-Scheme $scheme;
+     proxy_cache cache;
+     proxy_cache_bypass $cookie_auth_tkt;
+     proxy_no_cache $cookie_auth_tkt;
+     proxy_cache_valid 30m;
+     proxy_cache_valid 404 5m;
+  }
+
+  location ~ /open-data/[a-zA-z][a-zA-z]/data$ {
+     proxy_pass http://0.0.0.0:8008;
+     proxy_set_header Host $host:80;
+     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+     proxy_set_header X-Scheme $scheme;
+     proxy_cache cache;
+     proxy_cache_bypass $cookie_auth_tkt;
+     proxy_no_cache $cookie_auth_tkt;
+     proxy_cache_valid 30m;
+     proxy_cache_valid 404 5m;
+  }
+
+  location /open-data/ {
+     proxy_pass http://0.0.0.0:8008;
+     proxy_set_header Host $host:80;
+     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+     proxy_set_header X-Scheme $scheme;
   }
 }
-
 EOF
   
   echo 'Installing into $CKAN_APPLICATION'
@@ -86,5 +145,4 @@ EOF
   chkconfig --list nginx
   
   $CKAN_APPLICATION/init.d/nginx restart
-  
 }
