8267604856bd74f896172ab0dd749f8696f0df4d
John Glover <j@johnglover.net>
2012-07-10 10:32:45 +0100

    Add script to download dependencies (that aren't in RHEL repos) to 'downloads' folder.

---
+++ scripts/download_dependencies.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+## Usage:
+##
+## Source this file, and run download_dependencies()
+
+download_dependencies () {
+  sudo yum install wget
+
+  echo '------------------------------------------'
+  echo 'Downloading Dependency Files              '
+  echo '------------------------------------------'
+
+  PREV_DIR=$PWD
+  cd ../downloads
+
+  # echo 'Downloading solr'
+  # wget http://mirrors.ukfast.co.uk/sites/ftp.apache.org/lucene/solr/1.4.1/apache-solr-1.4.1.tgz
+
+  # echo 'Downloading EC ODP multilingual solr files'
+  # git clone https://github.com/okfn/ckanext-ecportal.git
+  # mv ckanext-ecportal/ckanext/ecportal/solr solr_schema
+  # tar -cf solr_schema.tar.gz solr_schema
+  # rm -rf solr_schema
+  # rm -rf ckanext-ecportal
+
+  echo 'Downloading elastic search'
+  wget https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.19.4.tar.gz
+
+  cd ../rpms
+  echo 'Downloading nginx rpms'
+
+  # download rpms for both 64 and 32 bit, decide which to use when installing
+  wget http://nginx.org/packages/rhel/6/i386/RPMS/nginx-1.2.2-1.el6.ngx.i386.rpm
+  wget http://nginx.org/packages/rhel/6/x86_64/RPMS/nginx-1.2.2-1.el6.ngx.x86_64.rpm
+
+  cd $PREV_DIR
+}
