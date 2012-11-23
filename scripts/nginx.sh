89d281a8a6934da0132f8bdf9584d0d660df3278
Ian Murray <ian.c.murray@gmail.com>
2012-11-23 12:58:53 +0000

    Escape nginx variables
    So they are not interpretted as shell variables.

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -97,12 +97,12 @@ http {
     include       /etc/nginx/mime.types;
     default_type  application/octet-stream;
 
-    log_format  main  '$remote_addr - $remote_user [$time_local] '
-                      '$upstream_cache_status '
-                      '$upstream_http_cache_control '
-                      '"$request" '
-                      '$status $body_bytes_sent "$http_referer" '
-                      '"$http_user_agent" "$http_x_forwarded_for"';
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] '
+                      '\$upstream_cache_status '
+                      '\$upstream_http_cache_control '
+                      '"\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
 
     access_log  /var/log/nginx/access.log  main;
 
